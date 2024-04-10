61707201cafa6be3b81d02f1ce53500db02d1a83
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-04-09 23:51:07 -0600

    run character

--- index.js
+++ index.js
@@ -40,6 +40,17 @@ const player = new Fighter({
     x: 210,
     y: 157,
   },
+  sprites: {
+    idle: {
+      imageSrc: "/samuraiMack/Idle.png",
+      framesMax: 8,
+    },
+    run: {
+      imageSrc: "/samuraiMack/Run.png",
+      framesMax: 8,
+      image: new Image(),
+    },
+  },
 });
 
 const enemy = new Fighter({
@@ -95,10 +106,13 @@ function animate() {
   enemy.velocity.x = 0;
 
   //player key movement
+  player.image = player.sprites.idle.image;
   if (keys.a.pressed && player.lastKey === "a") {
     player.velocity.x = -5;
+    player.image = player.sprites.run.image;
   } else if (keys.d.pressed && player.lastKey === "d") {
     player.velocity.x = 5;
+    player.image = player.sprites.run.image;
   }
 
   //enemy movement
